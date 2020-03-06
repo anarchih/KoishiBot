739518c5b75e73d428d38dde3d89307b610d5312
hychih <anarchih.tw@gmail.com>
2020-03-06 19:51:43 +0800

    add a function for validating the string is url or not

--- utils.py
+++ utils.py
@@ -1,8 +1,20 @@
 import discord
+import re
 
+
+is_url_regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 async def send_image(channel, path, content=None):
     with open(path, 'rb') as f:
         picture = discord.File(f)
         await channel.send(content=content, file=picture)
     pass
+
+def is_url(url):
+    return re.match(is_url_regex, url) is not None
