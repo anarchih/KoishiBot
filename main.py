a8513d459ffe18bdd83a68952b997398a7d00306
hychih <anarchih.tw@gmail.com>
2020-03-09 15:33:29 +0800

    avoid help command showing preview

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def run_command(message):
             r = random.randint(0, len(cmd_list) - 2)
             await message.channel.send(cmd_list[r + 1])
     elif cmd0 == "help":
-        await message.channel.send("https://www.notion.so/Koishi-Discord-Bot-b4fea743c6d049fab8d8e1a1c768f4d5")
+        await message.channel.send("<https://www.notion.so/Koishi-Discord-Bot-b4fea743c6d049fab8d8e1a1c768f4d5>")
 @client.event
 async def on_ready():
     pass
