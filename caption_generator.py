6b5dd481153a6472b4bb2c1d48d2feb08dae3dea
hychih <anarchih.tw@gmail.com>
2020-03-18 23:37:22 +0800

    Redesign BaseCaption

--- caption_generator.py
+++ caption_generator.py
@@ -6,6 +6,7 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import io
+import numpy as np
 
 
 def add_margin(pil_img, top, right, bottom, left, color):
@@ -17,40 +18,42 @@ def add_margin(pil_img, top, right, bottom, left, color):
     return result
 
 def get_center(x, y, w, h):
-    return x - w / 2, y - h / 2
+    return x - w / 2, (y - h / 2) - h * 0.1
 
 def get_font_by_size_searching(draw, text, _min, _max, width, height):
     for size in range(_min, _max + 2):
-        font = ImageFont.truetype("font/NotoSansCJK-Regular.ttc", size, encoding="unic")
+        font = ImageFont.truetype("fonts/NotoSansCJK-Regular.ttc", size, encoding="unic")
         w, h = draw.textsize(text, font=font)
         if w > width or h > height:
             break
     if size == _min:
         return None
 
-    font = ImageFont.truetype("font/NotoSansCJK-Regular.ttc", size - 1, encoding="unic")
+    font = ImageFont.truetype("fonts/NotoSansCJK-Regular.ttc", size - 1, encoding="unic")
     return font
 
-class ImageText(object):
-    def __init__(self, cmd_keys=["say"]):
-        self.cmd_keys = cmd_keys
-        self.margin_top_w, self.margin_top_b = 0, 80
-        self.margin_right_w, self.margin_right_b = 0, 40
-        self.margin_bottom_w, self.margin_bottom_b = 0, 0
-        self.margin_left_w, self.margin_left_b = 0, 40
 
-        self.margin_color = (255, 255, 255, 255)
+class BaseCaption(object):
+    def __init__(self, cmd_keys=["caption"]):
+        self.cmd_keys = cmd_keys
+        self.margin_top_w, self.margin_top_b = 0, 0
+        self.margin_right_w, self.margin_right_b = 0, 0
+        self.margin_bottom_w, self.margin_bottom_b = 0, 0
+        self.margin_left_w, self.margin_left_b = 0, 0
+
+        self.margin_color = (0, 0, 0, 255)
         self.text_align = "center"
-        self.text_color = (0, 0, 0, 0)
-        self.text_width_w, self.text_width_b = .9, 0
-        self.text_height_w, self.text_height_b = 0, self.margin_top_b * .9
+        self.text_color = (255, 255, 255, 255)
+        self.text_width_w, self.text_width_b = 1, 0
+        self.text_height_w, self.text_height_b = 1, 0
         self.text_min_size, self.text_max_size = 8, 36
         self.text_x_w, self.text_x_b = .5, 0
-        self.text_y_w, self.text_y_b = .0, self.margin_top_b / 2
+        self.text_y_w, self.text_y_b = .5, 0
+        self.text_spacing = 0
 
     def get_image(self, arg, message):
-        filename = "annoy.png"
-        return Image.open(filename)
+        return Image.new("RGBA", (100, 100), "black")
+        draw.text()
 
     def add_margin(self, img):
         W, H = img.size
@@ -76,7 +79,7 @@ class ImageText(object):
             self.text_height_w * H + self.text_height_b
         )
         if not font:
-            message = "Too many lines or too many characters in single line."
+            message = "Too many lines or too many characters in one line."
         return font, message
 
     def set_font(self, img, draw, text, font):
@@ -93,7 +96,8 @@ class ImageText(object):
             text,
             self.text_color,
             font=font,
-            align=self.text_align
+            align=self.text_align,
+            spacing = self.text_spacing
         )
 
     async def send_image(self, img, channel):
@@ -105,7 +109,6 @@ class ImageText(object):
 
     async def on_command(self, cmd, args, message):
         if cmd in self.cmd_keys:
-            print(args)
             if len(args) >= 1:
                 text = args[0].replace("\\n", "\n")
                 img = self.get_image(args[0], message)
